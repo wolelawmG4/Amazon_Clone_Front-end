849dfbc4af079265c4794d56331006793416edaf
Wolela Woldemicheal Woldeselassie <wolelawm050@gmail.com>
2024-12-06 17:10:58 +0300

    Router.jsx updated for deployment

--- src/Router.jsx
+++ src/Router.jsx
@@ -23,7 +23,7 @@ function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path="/Amazon_Clone_Front-end" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
         <Route
           path="/payments"
