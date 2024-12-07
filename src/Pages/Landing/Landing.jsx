f2c508fc1cdf5498309fd1cf10e7a8b87136df5a
Wolela Woldemicheal Woldeselassie <wolelawm050@gmail.com>
2024-12-07 18:37:13 +0300

    debugging paragraph added

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import LayOut from '../../Components/LayOut/LayOut'
-import Carousel from '../../Components/Carousel/CarouselEffect'
-import Category from '../../Components/Category/Category'
-import Product from '../../Components/Product/Product'
+import React from "react";
+import LayOut from "../../Components/LayOut/LayOut";
+import Carousel from "../../Components/Carousel/CarouselEffect";
+import Category from "../../Components/Category/Category";
+import Product from "../../Components/Product/Product";
 function Landing() {
   return (
     <LayOut>
-        <Carousel/>
-        <Category/>
-        <Product/>
+      <div>
+        <p>this is landding page </p>
+      </div>
+      <Carousel />
+      <Category />
+      <Product />
     </LayOut>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
