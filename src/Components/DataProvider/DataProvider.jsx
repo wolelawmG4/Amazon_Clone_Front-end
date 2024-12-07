95597cd57d6d44de95d032aeb500f74f70909063
Wolela Woldemicheal Woldeselassie <wolelawm050@gmail.com>
2024-12-07 18:21:55 +0300

    some debuging console logs added

--- src/Components/DataProvider/DataProvider.jsx
+++ src/Components/DataProvider/DataProvider.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useReducer } from "react";
 
 export const DataContext = createContext();
+console.log("DataProvider is rendering...");
+
 
 export const DataProvider = ({ children, reducer, initialState }) => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./Components/DataProvider/DataProvider";
 import { initialState, reducer } from "./Utility/reducer";
 import App from "./App.jsx";
-
+console.log("Rendering App...");
 
 createRoot(document.getElementById("root")).render(
   // <BrowserRouter future={{ v7_startTransition: true }}>
